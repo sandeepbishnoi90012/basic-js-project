86d19b6cd764b0a6338dd9917f9ff32b1aea130e
sandeep <sandeepbishnoi90012@gmail.com>
2023-02-09 21:17:51 +0530

    pragraph generator upgraded

--- random paragraph generator/main.js
+++ random paragraph generator/main.js
@@ -5,7 +5,7 @@ document.getElementById('btn').addEventListener('click',()=>{
     for(let i = 1;i <= paraNo;i++){
         let p =  document.createElement('p')
         p.classList.add('parabox');
-        p.innerText = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime id, quibusdam eius deserunt molestiae deleniti labore necessitatibus, exercitationem sunt odit perferendis laborum ullam sed quam ab hic commodi? Molestiae odit deleniti ipsa ipsam totam molestias`
+        p.innerText = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime id, quibusdam eius deserunt molestiae deleniti labore necessitatibus, exercitationem sunt odit perferendis laborum ullam sed quam ab hic commodi? Molestiae odit deleniti ipsa ipsam totam molestias psum dolor sit amet consectetur, adipisicing elit. Maxime id, quibusdam eius deserunt molestiae deleniti labore necessitatibus, exercitationem sunt odit perferendis laborum ullam sed quam ab hic commodi? Molestiae odit deleniti ipsa ipsam totam molestias psum dolor sit amet consectetur, adipisicing elit. Maxime id, quibusdam eius deserunt molestiae deleniti labore necessitatibus, exercitationem sunt odit perferendis laborum ullam sed quam ab hic commodi? Molestiae odit deleniti ipsa ipsam totam molestias`
        mainbox.append(p)
        console.log(mainbox)
     }
